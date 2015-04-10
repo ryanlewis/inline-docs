0d1120d9b1600bc47fe190c424994d8e37617509
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2015-04-10 11:30:03 +0100

    grunt-bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -188,7 +188,27 @@ module.exports = function (grunt) {
           globals: {}
         }
       }
+    },
+
+    bump: {
+      options: {
+        files: ['package.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json'],
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin',
+        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+        globalReplace: false,
+        prereleaseName: false,
+        regExp: false
+      }
     }
+
   });
 
   grunt.registerTask('default', ['jshint', 'concat', 'less', 'copy:config', 'copy:views']);
